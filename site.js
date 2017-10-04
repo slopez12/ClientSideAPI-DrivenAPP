35503bbbd7b7e0cf9fccbf8e40e7bd2f1bfa2441
Sara Victoria Lopez <slopez12@hawk.iit.edu>
2017-10-04 18:20:38 -0500

    Organized and parse data

--- site.js
+++ site.js
@@ -1,11 +1,22 @@
 var data;
 var num=0;
 $('#artistform').on('submit', function(e) {
-  var artist = $('#inputtext').val();
-  var url = 'https://itunes.apple.com/search?term=' + artist;
-  $.get(url, function(data) {
-    var aristinfo = data.item[0].artistinfo;
-    $('results').append('<li><a>')
+  console.log("Form submitted!");
+  var artistname = $('#artistname').val();
+  console.log("Artist name is " + artistname);
+  var artist_url = 'https://itunes.apple.com/search?term=' + artistname;
+  console.log("Artist URL is " + artist_url);
+
+  $.get(artist_url, function(data) {
+    data = JSON.parse(data);
+    console.log(typeof(data));
+    $('#results').append('<li><a href="'+data['results'][0]['artistViewUrl']+'">' + data['results'][0]['artistName'] + '</a></li>');
+  });
 
   e.preventDefault();
-}});
+});
+
+
+$('#artistname').on('focus', function(){
+  console.log('It works!');
+})
