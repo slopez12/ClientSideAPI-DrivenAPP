c9a60926013febae2b28b61acc26d1bc29366c0c
Sara Lopez <slopez12@hawk.iit.edu>
2017-10-12 19:12:47 -0500

    Add a loop, so I think

--- site.js
+++ site.js
@@ -13,11 +13,17 @@ $('#artistform').on('submit', function(e) {
     $('#results').append('<li><a href="'+data['results'][0]['artistViewUrl']+'">' +
     data['results'][0]['artistName'] + '</a></li>');
     console.log("Artist Url is " + 'artistViewUrl');
-    $('#results').append('<li><a href="'+data['results'][0]['previewUrl']+'">' + '</a></li>');
-    console.log("Artist's track preview URL is " + 'previewUrl');
     $('#results').append('<li><a href="'+data['results'][0]['trackViewUrl']+'">' +
     data['results'][0]['trackName'] + '</a></li>');
     console.log("Artist track URL is " + 'trackViewUrl');
+    var trackName = data['trackName'];
+
+    for (var trackName in data) {
+     if (data.hasOwnProperty(trackName))
+}
+    $('#results').append('<li><a href="'+data['results'][0]['previewUrl']+'">' + '</a></li>');
+    console.log("Artist's track preview URL is " + 'previewUrl');
+
 
   });
 
